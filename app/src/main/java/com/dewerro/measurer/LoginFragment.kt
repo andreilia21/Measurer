9c17ca9536e4841440b980605d5da2458b10d05b
Dewerro <funcake0@gmail.com>
2022-11-18 19:01:21 +0300

    Add sign up fragment

--- app/src/main/java/com/dewerro/measurer/LoginFragment.kt
+++ app/src/main/java/com/dewerro/measurer/LoginFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.dewerro.measurer.databinding.FragmentLoginBinding
 
 /**
@@ -31,6 +32,13 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        signUpButton()
+    }
+
+    private fun signUpButton() {
+        binding.signUpButton.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_SignupFragment)
+        }
     }
 
     override fun onDestroyView() {
