54ded1ed1f1cb079ae143f73b6f0918e5e258e3c
Dewerro <funcake0@gmail.com>
2022-11-20 16:47:22 +0300

    Add user sign out

--- app/src/main/java/com/dewerro/measurer/SelectImageFragment.kt
+++ app/src/main/java/com/dewerro/measurer/SelectImageFragment.kt
@@ -2,18 +2,15 @@ package com.dewerro.measurer
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast.LENGTH_LONG
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.dewerro.measurer.databinding.FragmentSelectImageBinding
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SelectImageFragment : Fragment() {
@@ -54,6 +51,11 @@ class SelectImageFragment : Fragment() {
 
         binding.measureWithImage.setOnClickListener {
             galleryLauncher.launch("image/*")
+        }
+
+        binding.logoutButton.setOnClickListener {
+            Firebase.auth.signOut()
+            findNavController().navigate(R.id.action_SelectImageFragment_to_LoginFragment)
         }
     }
 
