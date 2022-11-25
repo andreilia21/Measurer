06cb93f396ec4c1af6faab5b99a29e9f8b3d51e4
Dewerro <funcake0@gmail.com>
2022-11-25 17:45:27 +0300

    Add constant values

---
+++ app/src/main/java/com/dewerro/measurer/Constants.kt
@@ -0,0 +1,18 @@
+package com.dewerro.measurer
+
+/**
+ * Константные значения
+ */
+object K {
+    object SharedPreferences {
+        const val FIREBASE_USER_DATA = "user_data"
+        const val FIREBASE_EMAIL = "email"
+        const val FIREBASE_PASSWORD = "password"
+    }
+    object Bundle {
+        const val GALLERY_IMAGE_URI = "imageURI"
+    }
+    object Firebase {
+        const val DOORS_COLLECTION_NAME = "doors"
+    }
+}
